434ac0bf720767a91abf26da82fce10952dace22
MezianeKhalil <khalilmez2000@gmail.com>
2023-10-06 19:08:21 +0100

    feat: minor change add button to second stack

--- src/pages/layouts/FullWidthDoubleStack.tsx
+++ src/pages/layouts/FullWidthDoubleStack.tsx
@@ -1,5 +1,5 @@
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Divider, Flex, HStack, Heading, IconButton, Text, useColorModeValue } from '@chakra-ui/react'
-import { AiOutlineUser } from 'react-icons/ai'
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Divider, Flex, HStack, Heading, IconButton, Text, useColorModeValue } from '@chakra-ui/react'
+import { AiOutlineUser, AiOutlinePlus } from 'react-icons/ai'
 import { IoEarthOutline } from 'react-icons/io5'
 
 import { Head, PreviewOptionsNavbar } from '@components/index'
@@ -19,23 +19,26 @@ export default function FullWidthDoubleStack() {
           <IconButton isRound={true} size="lg" aria-label='user icon' icon={<AiOutlineUser />}  />
         </HStack>
       </Flex>
-      <HStack alignItems="center" spacing="4" h='16' p='3' borderBottom="1px" borderColor={useColorModeValue('gray.200', 'gray.600')}>
-        <Heading as="h2" size="md">Overview</Heading>
-        <Divider orientation='vertical' />
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#'>Home</BreadcrumbLink>
-          </BreadcrumbItem>
+      <Flex justifyContent="space-between" gap="4" h='16' p='3' borderBottom="1px" borderColor={useColorModeValue('gray.200', 'gray.600')}>
+        <HStack alignItems="center">
+          <Heading as="h2" size="md">Overview</Heading>
+          <Divider orientation='vertical' />
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#'>Docs</BreadcrumbLink>
-          </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='#'>Docs</BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink href='#'>Breadcrumb</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
-      </HStack>
+            <BreadcrumbItem isCurrentPage>
+              <BreadcrumbLink href='#'>Breadcrumb</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </HStack>
+        <Button leftIcon={<AiOutlinePlus />} colorScheme='blue'>New Project</Button>
+      </Flex>
       <Flex as="main" minH="78vh" align="center" justify="center" bg={useColorModeValue('gray.50', 'gray.900')}>
         <Box textAlign="center">
           <Heading as='h3'>Box Heading</Heading>
