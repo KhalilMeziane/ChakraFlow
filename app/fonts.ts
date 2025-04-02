375ab17774793e8ce6eed60f0060ef940dac2bb7
khalil meziane <khalilmez2000@gmail.com>
2025-04-02 12:45:04 +0100

    feat: setup chakraui

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: '400'
+})
+
+export const fonts = {
+  poppins,
+}
