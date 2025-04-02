2bb4a96398207591ff9877c31a3571b1f4d64ae6
khalil meziane <khalilmez2000@gmail.com>
2025-04-02 22:03:49 +0100

    feat: add openGraph meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,20 @@ export const metadata: Metadata = {
 	description: "The Ultimate Chakra UI Dashboard Template and Toolkit",
 	keywords:
 		"chakra ui dashboard, chakra ui components, next.js dashboard, chakra ui admin panel, chakra ui layout, chakra ui sidebar, chakra ui navbar, chakra ui cards, chakra ui tables, chakra ui widgets, chakra ui charts, chakra ui statistics, chakra ui modals, chakra ui forms, chakra ui buttons, chakra ui theme, chakra ui dark mode, chakra ui ui kit, chakra ui responsiveness, chakra ui best practices, chakra ui customization",
+	openGraph: {
+		title: "Chakra Flow",
+		description: "The Ultimate Chakra UI Dashboard Template and Toolkit",
+		url: "https://chakra-flow.vercel.app",
+		type: "website",
+		images: [
+			{
+				url: "https://chakra-flow.vercel.app/thumbnail.png",
+				width: 1200,
+				height: 630,
+				alt: "Chakra Flow Dashboard Thumbnail",
+			},
+		],
+		},
 };
 
 import { Providers } from "./providers";
